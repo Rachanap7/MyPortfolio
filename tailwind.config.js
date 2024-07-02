788b5512fe06fec621145b563cbb910bc587c1c7
Rachana <rachana.parkar711@gmail.com>
2024-07-02 22:42:53 +0530

    css updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ module.exports = {
         '13.7rem':'13.7rem',
       },
       boxShadow:{
-        'custom':"rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;",
+        'custom':"rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
       },
       backgroundColor: {
         'rgba-white-0.3': 'rgba(255, 255, 255, 0.3)',
